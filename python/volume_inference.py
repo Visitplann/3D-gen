dc1d0112542bd4a40db847c2a7fb40f1177fcc2f
Irinahann <74796438+Irinahann@users.noreply.github.com>
2026-03-24 10:42:20 +0000

    added the new volume_inference from the other branch(in comment)

--- python/volume_inference.py
+++ python/volume_inference.py
@@ -21,3 +21,39 @@ def infer_volumes(shapes, view):
     volumes.append(volume)
     
   return volumes
+
+
+"""import cv2
+
+def infer_volumes(shapes, view_type):
+
+  volumes = []
+
+  is_top =  view_type == "top"
+  is_side = view_type in ["left", "right", "front", "back"]
+
+  for shape in shapes:
+
+    if cv2.contourArea(shape) < 1000:
+        continue
+
+    if is_top:
+      volume = {
+          "type": "footprint",
+          "contour": shape
+      }
+    elif is_side:
+      _, _, _, h = cv2.boundingRect(shape)
+
+      volume = {
+          "type": "profile",
+          "height": h,
+          "contour": shape
+      }
+    else:
+      # fallback
+      continue
+
+    volumes.append(volume)
+
+  return volumes"""
